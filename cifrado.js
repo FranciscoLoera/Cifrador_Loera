02f22e24fac46ca3b042303ceb5cf0e70ad0dfc4
Francisco Loera <114623243+FranciscoLoera@users.noreply.github.com>
2023-03-27 18:28:44 -0700

    update

--- cifrado.js
+++ cifrado.js
@@ -1,7 +1,7 @@
 // Función para Encriptar el texto
 const encriptar = () => {
     var texto1 = document.getElementById("texto1").value;
-    var clave = "AldoGuapo"; // clave secreta
+    var clave = document.getElementById("clave").value; // clave secreta
     var textoEncriptado = CryptoJS.AES.encrypt(texto1, clave).toString();
     document.getElementById("resultado1").innerHTML = textoEncriptado;
 }
@@ -9,7 +9,7 @@ const encriptar = () => {
 // Función para desencriptar el texto
 const desencriptar = () => {
     var texto2 = document.getElementById("texto2").value;
-    var clave = "AldoGuapo"; // clave secreta
+    var clave = document.getElementById("clave").value; // clave secreta
     var textoDesencriptado = CryptoJS.AES.decrypt(texto2, clave).toString(CryptoJS.enc.Utf8);
     document.getElementById("resultado2").innerHTML = textoDesencriptado;
 }
